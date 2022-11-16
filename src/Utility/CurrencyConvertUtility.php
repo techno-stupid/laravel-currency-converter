5b1c02b891a02cff079dfe4bf558ecdfc4e1b4f2
Md. Mahady Hasan Pial <pial.coder@gmail.com>
2022-11-16 16:29:26 +0600

    Currency Convertion functionality

--- src/Utility/CurrencyConvertUtility.php
+++ src/Utility/CurrencyConvertUtility.php
@@ -7,7 +7,11 @@ class CurrencyConvertUtility
     public static function convert($amount,$currencyTo='USD',$currencyFrom='USD')
     {
         try{
-            return $amount*3;
+            $exchangeRates =  json_decode(file_get_contents('http://cdn.moneyconvert.net/api/latest.json'),true)['rates'];
+            if($amount <= 0) return 'Amount should be a positive integer';
+            if(!array_key_exists($currencyTo, $exchangeRates) || !array_key_exists($currencyFrom, $exchangeRates)) return 'Invalid Currency';
+            $exchangedAmount = ($amount / $exchangeRates[$currencyTo]) * $exchangeRates[$currencyFrom];
+            return $exchangedAmount;
         }catch (\Exception $e) {
             return $e->getMessage();
         }
